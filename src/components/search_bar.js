3de230d946987045d1136b37c569eba044f96638
Trachifab <fabiendorey@gmail.com>
2017-05-08 18:24:06 +0200

    Testing state and value binding

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SeachBar = () => {
-    return <input />;
-};
+class SeachBar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div> 
+                <input
+                    value={this.state.term} 
+                    onChange={(event) => this.setState({ term: event.target.value })} 
+                />;
+            </div>
+        );
+    }
+
+
+}
 
 export default SeachBar;
